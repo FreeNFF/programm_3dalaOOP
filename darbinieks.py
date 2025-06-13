363088f8794690d53a2f4b9267086c1d0579fda1
FreeNFF <nauisfricsons30@gmail.com>
2025-06-13 10:54:36 +0300

    Create darbinieks.py

---
+++ darbinieks.py
@@ -0,0 +1,26 @@
+class Darbinieks():
+    def __init__(self, vards, uzvards):
+        self.vards = vards
+        self.uzvards = uzvards
+
+    def izvade(self):
+        return print(f"Darbinieka vārds un uzvārds: {self.vards} {self.uzvards}")
+    
+    def bruto(self):
+        pass
+    
+    def neto(self):
+        return 
+    
+class OfisaDarbinieks(Darbinieks):
+    def __init__(self, vards, uzvards, nauda_stunda, stundas):
+        super.__init__(vards,uzvards)
+        self.nauda_stundas = nauda_stunda
+        self.stundas = stundas
+
+    def 
+
+    def bruto(self)
+
+
+        
